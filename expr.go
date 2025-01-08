fdb3d664b48ab00f92e1fbd6c7b339b91bd3ab48
Glen Haggin <glen.haggin@uber.com>
2025-01-07 16:36:51 -0800

    evaluate expressions

--- expr.go
+++ expr.go
@@ -4,56 +4,156 @@ import "fmt"
 
 type Expr interface {
 	Print() string
+	Evaluate() any
 }
 
+func Parenthesize(name string, exprs ...Expr) string {
+	s := "(" + name
+	for _, expr := range exprs {
+		s += " " + expr.Print()
+	}
+	return s + ")"
+}
+
+func isTruthy(v any) bool {
+	if v == nil {
+		return false
+	}
+
+	if b, ok := v.(bool); ok {
+		return b
+	}
+
+	return true
+}
+
+func isEqual(l, r any) bool {
+	if l == nil && r == nil {
+		return true
+	}
+
+	if l == nil || r == nil {
+		return false
+	}
+
+	return l == r
+}
+
+// AssignExpr ///////////////////////////////////
 type AssignExpr struct {
 	Name  Token
 	Value Expr
 }
 
-func (a AssignExpr) Print() string {
+func (expr AssignExpr) Evaluate() any {
+	return nil
+}
+
+func (expr AssignExpr) Print() string {
 	return "<print-not-implemented>"
 }
 
+// BinaryExpr ///////////////////////////////////
 type BinaryExpr struct {
 	Op    Token
 	Left  Expr
 	Right Expr
 }
 
+func (expr BinaryExpr) Evaluate() any {
+	left := expr.Left.Evaluate()
+	right := expr.Right.Evaluate()
+
+	switch expr.Op.Type {
+	case BANG_EQUAL:
+		return !isEqual(left, right)
+	case EQUAL_EQUAL:
+		return isEqual(left, right)
+	case GREATER:
+		return left.(float64) > right.(float64)
+	case GREATER_EQUAL:
+		return left.(float64) >= right.(float64)
+	case LESS:
+		return left.(float64) < right.(float64)
+	case LESS_EQUAL:
+		return left.(float64) <= right.(float64)
+	case MINUS:
+		return left.(float64) - right.(float64)
+	case SLASH:
+		return left.(float64) / right.(float64)
+	case STAR:
+		return left.(float64) * right.(float64)
+	case PLUS:
+		leftString, leftOK := left.(string)
+		rightString, rightOK := right.(string)
+		if leftOK && rightOK {
+			return leftString + rightString
+		}
+
+		leftFloat, leftOK := left.(float64)
+		rightFloat, rightOK := right.(float64)
+		if leftOK && rightOK {
+			return leftFloat + rightFloat
+		}
+	}
+
+	// unreachable
+	return nil
+}
+
 func (expr BinaryExpr) Print() string {
 	return Parenthesize(expr.Op.Lexeme, expr.Left, expr.Right)
 }
 
+// CallExpr /////////////////////////////////////
 type CallExpr struct {
 	Callee Expr
 	Paren  Token
 	Args   []Expr
 }
 
+func (expr CallExpr) Evaluate() any {
+	return nil
+}
+
 func (expr CallExpr) Print() string {
 	return "<print-not-implemented>"
 }
 
+// GetExpr //////////////////////////////////////
 type GetExpr struct {
 	Object Expr
 	Name   Token
+}
+
+func (expr GetExpr) Evaluate() any {
+	return nil
 }
 
 func (expr GetExpr) Print() string {
 	return "<print-not-implemented>"
 }
 
+// GroupingExpr /////////////////////////////////
 type GroupingExpr struct {
 	Expression Expr
+}
+
+func (expr GroupingExpr) Evaluate() any {
+	return nil
 }
 
 func (expr GroupingExpr) Print() string {
 	return Parenthesize("group", expr.Expression)
 }
 
+// LiteralExpr //////////////////////////////////
 type LiteralExpr struct {
 	Value any
+}
+
+func (expr LiteralExpr) Evaluate() any {
+	return expr.Value
 }
 
 func (expr LiteralExpr) Print() string {
@@ -63,64 +163,97 @@ func (expr LiteralExpr) Print() string {
 	return fmt.Sprintf("%v", expr.Value)
 }
 
+// LogicalExpr //////////////////////////////////
 type LogicalExpr struct {
 	Left  Expr
 	Right Expr
 	Op    Token
 }
 
+func (expr LogicalExpr) Evaluate() any {
+	return nil
+}
+
 func (expr LogicalExpr) Print() string {
 	return "<print-not-implemented>"
 }
 
+// SetExpr //////////////////////////////////////
 type SetExpr struct {
 	Object Expr
 	Name   Token
 	Value  Expr
 }
 
+func (expr SetExpr) Evaluate() any {
+	return nil
+}
+
 func (expr SetExpr) Print() string {
 	return "<print-not-implemented>"
 }
 
+// SuperExpr ////////////////////////////////////
 type SuperExpr struct {
 	Keyword Token
 	Method  Token
+}
+
+func (expr SuperExpr) Evaluate() any {
+	return nil
 }
 
 func (expr SuperExpr) Print() string {
 	return "<print-not-implemented>"
 }
 
+// ThisExpr /////////////////////////////////////
 type ThisExpr struct {
 	Keyword Token
+}
+
+func (expr ThisExpr) Evaluate() any {
+	return nil
 }
 
 func (expr ThisExpr) Print() string {
 	return "<print-not-implemented>"
 }
 
+// UnaryExpr ////////////////////////////////////
 type UnaryExpr struct {
 	Op    Token
 	Right Expr
+}
+
+func (expr UnaryExpr) Evaluate() any {
+	right := expr.Right.Evaluate()
+
+	switch expr.Op.Type {
+	case MINUS:
+		// TODO: what if right is not a float64?
+		return -(right.(float64))
+	case BANG:
+		return !isTruthy(right)
+	}
+
+	// unreachable
+	return nil
 }
 
 func (expr UnaryExpr) Print() string {
 	return Parenthesize(expr.Op.Lexeme, expr.Right)
 }
 
+// VariableExpr /////////////////////////////////
 type VariableExpr struct {
 	Name Token
 }
 
-func (expr VariableExpr) Print() string {
-	return "<print-not-implemented>"
+func (expr VariableExpr) Evaluate() any {
+	return nil
 }
 
-func Parenthesize(name string, exprs ...Expr) string {
-	s := "(" + name
-	for _, expr := range exprs {
-		s += " " + expr.Print()
-	}
-	return s + ")"
+func (expr VariableExpr) Print() string {
+	return "<print-not-implemented>"
 }
--- expr_test.go
+++ expr_test.go
@@ -44,3 +44,279 @@ func TestPrettyPrint(t *testing.T) {
 		})
 	}
 }
+
+func TestIsTruthy(t *testing.T) {
+	testCases := []struct {
+		name     string
+		v        any
+		expected bool
+	}{
+		{
+			name:     "nil",
+			v:        nil,
+			expected: false,
+		},
+		{
+			name:     "true",
+			v:        true,
+			expected: true,
+		},
+		{
+			name:     "false",
+			v:        false,
+			expected: false,
+		},
+		{
+			name:     "zero",
+			v:        0.,
+			expected: true,
+		},
+		{
+			name:     "non_zero",
+			v:        1.,
+			expected: true,
+		},
+		{
+			name:     "empty_string",
+			v:        "",
+			expected: true,
+		},
+		{
+			name:     "string",
+			v:        "hello world",
+			expected: true,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, isTruthy(tt.v))
+		})
+	}
+}
+
+func TestIsEqual(t *testing.T) {
+	testCases := []struct {
+		name     string
+		l, r     any
+		expected bool
+	}{
+		{
+			name:     "both nil",
+			l:        nil,
+			r:        nil,
+			expected: true,
+		},
+		{
+			name:     "one nil",
+			l:        nil,
+			r:        1.,
+			expected: false,
+		},
+		{
+			name:     "one nil",
+			l:        1.,
+			r:        nil,
+			expected: false,
+		},
+		{
+			name:     "floats_equal",
+			l:        1.,
+			r:        1.,
+			expected: true,
+		},
+		{
+			name:     "floats_not_equal",
+			l:        1.,
+			r:        2.,
+			expected: false,
+		},
+		{
+			name:     "strings_equal",
+			l:        "s1",
+			r:        "s1",
+			expected: true,
+		},
+		{
+			name:     "strings_not_equal",
+			l:        "s1",
+			r:        "s2",
+			expected: false,
+		},
+		{
+			name:     "string_not_equal_float",
+			l:        "s1",
+			r:        1.,
+			expected: false,
+		},
+		{
+			name:     "boolean_equal",
+			l:        true,
+			r:        true,
+			expected: true,
+		},
+		{
+			name:     "boolean_equal_2",
+			l:        false,
+			r:        false,
+			expected: true,
+		},
+		{
+			name:     "boolean_not_equal",
+			l:        true,
+			r:        false,
+			expected: false,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, isEqual(tt.l, tt.r))
+		})
+	}
+}
+
+func TestBinaryExpression(t *testing.T) {
+	testCases := []struct {
+		name     string
+		l        any
+		r        any
+		op       TokenType
+		expected any
+	}{
+		{
+			name:     "equal_equal",
+			l:        3.,
+			r:        5.,
+			op:       EQUAL_EQUAL,
+			expected: false,
+		},
+		{
+			name:     "bang_equal",
+			l:        3.,
+			r:        5.,
+			op:       BANG_EQUAL,
+			expected: true,
+		},
+		{
+			name:     "greater",
+			l:        3.,
+			r:        5.,
+			op:       GREATER,
+			expected: false,
+		},
+		{
+			name:     "greater_equal",
+			l:        3.,
+			r:        5.,
+			op:       GREATER_EQUAL,
+			expected: false,
+		},
+		{
+			name:     "less",
+			l:        3.,
+			r:        5.,
+			op:       LESS,
+			expected: true,
+		},
+		{
+			name:     "less_equal",
+			l:        3.,
+			r:        5.,
+			op:       LESS_EQUAL,
+			expected: true,
+		},
+		{
+			name:     "minus",
+			l:        3.,
+			r:        5.,
+			op:       MINUS,
+			expected: -2.,
+		},
+		{
+			name:     "slash",
+			l:        3.,
+			r:        5.,
+			op:       SLASH,
+			expected: 3. / 5.,
+		},
+		{
+			name:     "star",
+			l:        3.,
+			r:        5.,
+			op:       STAR,
+			expected: 3. * 5.,
+		},
+		{
+			name:     "plus_floats",
+			l:        3.,
+			r:        5.,
+			op:       PLUS,
+			expected: 3. + 5.,
+		},
+		{
+			name:     "plus_strings",
+			l:        "hello ",
+			r:        "world",
+			op:       PLUS,
+			expected: "hello world",
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, BinaryExpr{
+				Op: Token{
+					Type: tt.op,
+				},
+				Left: LiteralExpr{
+					Value: tt.l,
+				},
+				Right: LiteralExpr{
+					Value: tt.r,
+				},
+			}.Evaluate())
+		})
+	}
+}
+
+func TestUnaryEvaluate(t *testing.T) {
+	testCases := []struct {
+		name     string
+		op       TokenType
+		r        any
+		expected any
+	}{
+		{
+			name:     "minus",
+			op:       MINUS,
+			r:        3.,
+			expected: -3.,
+		},
+		{
+			name:     "bang",
+			op:       BANG,
+			r:        3.,
+			expected: false,
+		},
+		{
+			name:     "bang",
+			op:       BANG,
+			r:        false,
+			expected: true,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, UnaryExpr{
+				Op: Token{
+					Type: tt.op,
+				},
+				Right: LiteralExpr{
+					Value: tt.r,
+				},
+			}.Evaluate())
+		})
+	}
+}
